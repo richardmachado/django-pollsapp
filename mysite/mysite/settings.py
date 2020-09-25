105b12f8030fc34eb7583c1cef2d8b3d7f778236
Richard Machado <mmachado.richard@gmail.com>
2020-09-24 18:41:47 -0700

    removed key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_haws(a6w6b&@q3p*exljy5eb9t8cn&=(bv9^+5ngn4+z&x25q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
